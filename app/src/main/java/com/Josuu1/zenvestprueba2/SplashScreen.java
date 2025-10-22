863508a421d489a2a9907d71b8a44c2d6853ad21
Josue <jmzo17.jz@gmail.com>
2025-10-22 11:00:40 +0200

    Primer commit

---
+++ app/src/main/java/com/Josuu1/zenvestprueba2/SplashScreen.java
@@ -0,0 +1,68 @@
+package com.Josuu1.zenvestprueba2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+public class SplashScreen extends AppCompatActivity {
+
+    ImageView fondo;
+
+    ImageView splashLogo;
+
+    TextView nombreApp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Animation aparicion = AnimationUtils.loadAnimation(this, R.anim.aparecer);
+
+        nombreApp = findViewById(R.id.splashText);
+        nombreApp.startAnimation(aparicion);
+        splashLogo = findViewById(R.id.splashPerroNaranja);
+        splashLogo.startAnimation(aparicion);
+
+        fondo = findViewById(R.id.splashImagenFondo);
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1548199973-03cce0bbc87b?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+                .transition(DrawableTransitionOptions.withCrossFade(1000))
+                .centerCrop()
+                .into(fondo);
+
+        launchNextActivity();
+
+    }
+
+    public void launchNextActivity(){
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, Login.class);
+                startActivity(intent);
+            }
+        }, 2000);
+
+    }
+}
